49cf48fddf3839156f920d056109f5c44ec09812
ChrisOspina <ChrisOspina99@gmail.com>
2025-09-23 19:11:12 -0400

    Cover Letter backend actions

--- actions/cover-letter.js
+++ actions/cover-letter.js
@@ -3,6 +3,7 @@
 import { auth } from "@clerk/nextjs/server";
 import { db } from "@/lib/prisma";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { checkAuth } from "./check-auth";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({
@@ -10,17 +11,88 @@ const model = genAI.getGenerativeModel({
 })
 
 export async function generateCoverLetter(data){
+     const user = await checkAuth();
 
+     const prompt =`  Write a professional cover letter for a ${data.jobTitle} position at ${
+    data.companyName
+  }.
+    
+    About the candidate:
+    - Industry: ${user.industry}
+    - Years of Experience: ${user.experience}
+    - Skills: ${user.skills?.join(", ")}
+    - Professional Background: ${user.bio}
+    
+    Job Description:
+    ${data.jobDescription}
+    
+    Requirements:
+    1. Use a professional, enthusiastic tone
+    2. Highlight relevant skills and experience
+    3. Show understanding of the company's needs
+    4. Keep it concise (max 400 words)
+    5. Use proper business letter formatting in markdown
+    6. Include specific examples of achievements
+    7. Relate candidate's background to job requirements
+    
+    Format the letter in markdown.
+  `;
+
+  try{
+    //TODO cover letter generation logic
+    const result = await model.generateContent(prompt);
+    const content = result.response.text().trim();
+
+   const coverLetter = await db.coverLetter.create({
+      data: {
+        content,
+        jobDescription: data.jobDescription,
+        companyName: data.companyName,
+        jobTitle: data.jobTitle,
+        status: "completed",
+        userId: user.id,
+      },
+    });
+
+    return coverLetter;
+  }
+  catch(error){
+    console.error("Error generating cover letter");
+    throw new error("Failed to generate cover letter");
+  }
 }
 
 export async function getCoverLetters(){
+    const user = await checkAuth();
 
+    return await db.coverLetter.findMany({
+      where:{ 
+        userId: user.id,
+      },
+      orderBy:{
+        createdAt: "desc",
+      },
+    });
 }
 
 export async function getCoverLetter(id){
+    const user = await checkAuth();
 
+    return await db.coverLetter.findUnique({
+      where:{
+        id,
+        userId: user.id,
+      },
+    });
 }
 
 export async function deleteCoverLetter(id){
-    
+    const user = await checkAuth();
+
+    return await db.coverLetter.delete({
+      where:{
+        id,
+        userId: user.id,
+      }
+    })
 }
