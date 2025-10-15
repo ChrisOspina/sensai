5380066a56ae3c64e552726cf333fc42ba20f51d
ChrisOspina <ChrisOspina99@gmail.com>
2025-10-15 12:30:10 -0400

    using react to print for cover letter

--- app/(main)/cover-letter/components/cover-letter-preview.jsx
+++ app/(main)/cover-letter/components/cover-letter-preview.jsx
@@ -1,12 +1,71 @@
 "use client";
 
-import React from "react";
+import { Printer, Loader2 } from "lucide-react";
+import React, { useState, useRef } from "react";
 import MDEditor from "@uiw/react-md-editor";
+import { Button } from "@/components/ui/button";
+import { useReactToPrint } from "react-to-print";
 
 const CoverLetterPreview = ({ content }) => {
+  const [isGenerating, setIsGenerating] = useState(false);
+  const coverLetterRef = useRef();
+
+  const reactToPrintFn = useReactToPrint({
+    contentRef: coverLetterRef,
+    documentTitle: "cover letter",
+    removeAfterPrint: false,
+  });
+
+  const generatePDF = async () => {
+    setIsGenerating(true);
+    try {
+      console.log("printing");
+      await new Promise((resolve, reject) => {
+        try {
+          reactToPrintFn?.();
+          resolve();
+        } catch (error) {
+          reject(error);
+        }
+      });
+    } catch (error) {
+      console.error("PDF generation error:", error);
+      toast.error("This functionality is broken");
+    } finally {
+      setIsGenerating(false);
+    }
+  };
   return (
     <div className="py-4">
-      <MDEditor value={content} preview="preview" height={700} />
+      <div className="space-x-2">
+        <Button onClick={generatePDF} disabled={isGenerating}>
+          {isGenerating ? (
+            <>
+              <Loader2 className="h-4 w-4 animate-spin" />
+              Generating PDF...
+            </>
+          ) : (
+            <>
+              <Printer className="h-4 w-4" />
+              Print
+            </>
+          )}
+        </Button>
+      </div>
+      <div className="border rounded-lg">
+        <MDEditor value={content} preview="preview" height={700} />
+      </div>
+      <div className="hidden">
+        <div ref={coverLetterRef}>
+          <MDEditor.Markdown
+            source={content}
+            style={{
+              background: "white",
+              color: "black",
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
